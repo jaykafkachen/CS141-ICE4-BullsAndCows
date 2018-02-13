dbc95e9118b2d7377e8d6ad1a4d13aec8c3fa33a
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-13 14:04:15 -0800

    Create TwoFactorAuth.java

---
+++ TwoFactorAuth.java
@@ -0,0 +1,12 @@
+Class TwoFactorAuth extends Password
+{
+public TwoFactorAuth()
+{
+  super(generatePass()); //calls the superclass constructor, passing in the char[] from generate pass
+}
+
+public char[] generatePass() //overridden generatePass() method from superclass Password
+//in order to continue using char[] for the password, make sure to convert randomly generated ints to their character equivalents (heres the ASCII table again http://www.asciitable.com/)
+
+public boolean guess() //overridden from Superclass Password, prompts user to guess password til they get it (returns true if they do, false if they exit)
+}
