e90d700c1f9075c1ab66033e295caa65421f971d
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-13 14:35:41 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,19 +4,19 @@ class Driver
 {
   public static void main(String[] cheese)
   {
-    Scanner io = new Scanner(System.in);
+        Scanner io = new Scanner(System.in);
     char playFlag = 'y';
     boolean proceed = true;
     System.out.println("Welcome to the Password Guessing Game!");
     do
     { 
       Login lvlOne = new Login();
-      System.out.println("Level 1: Guess a random password of 8 characters, letters and numbers only (with hints):");
+      System.out.println("Level 1: Guess a random password of 8 characters, letters and numbers only with hints:");
       proceed = lvlOne.guess();
       if(proceed)
       {
         TwoFactorAuth lvlTwo = new TwoFactorAuth();
-        System.out.println("Congratulations! You may now advance to...\nLevel 2: Guess a random 3-5 digit authentication code (with hints)."
+        System.out.println("Congratulations! You may now advance to...\nLevel 2: Guess a random 3-5 digit authentication code with hints.");
         proceed = lvl2.guess();
       }
       if(proceed)
@@ -25,7 +25,7 @@ class Driver
         System.out.println("Give up? Better luck next time!");
         
       System.out.println("Would you like to play again? ('y'/'n')");
-      playFlag = io.nextLine.charAt(0);
-    } while(playFlag.toLowerCase()==('y'));
+      playFlag = io.next().charAt(0);
+    } while(Character.toLowerCase(playFlag)==('y'));
   }
 }
