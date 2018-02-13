c1271eea4bc39ff493b79365ab214d451f6443d0
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-13 14:53:01 -0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,11 +1,23 @@
-Class Login extends Password
+class Login extends Password
 {
   public Login()
   {
-    super(generatePass()); //calls abstract constructor to set password value to the created password
+    super();
   }
-  public char[] generatePass() //overridden generatePass() method from superclass Password
-  //in order to generate valid characters for password, use ints and RNG for ASCII values 97-122 (http://www.asciitable.com/), this generates a string of lowercase characters that you can guess essentially by playing hangman 
+  
+  //overridden generatePass() method from superclass Password
+  //in order to generate valid characters for password, use ints and RNG for ASCII values
+  //97-122 (http://www.asciitable.com/), this generates a char[] of lowercase characters
+  //that you can guess essentially by playing hangman
+  public char[] generatePass()
+  {
+    return null;
+  } 
 
-  public boolean guess() //overridden from Superclass Password, prompts user to guess password til they get it (returns true if they do, false if they exit)
+  public boolean guess()
+  {
+    return false;
+    
+  }//overridden from Superclass Password, prompts user to guess
+  //password til they get it (returns true if they do, false if they exit)
 }
