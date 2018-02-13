1b4f0d0199f0a1c3847c064bf9bbafb101fd4ed6
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-13 14:23:50 -0800

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Driver
+{
+  public static void main(String[] cheese)
+  {
+    Scanner io = new Scanner(System.in);
+    char playFlag = 'y';
+    boolean proceed = true;
+    Login lvlOne = new Login();
+    TwoFactorAuth lvlTwo = new TwoFactorAuth();
+    System.out.println("Welcome to the Password Guessing Game!");
+    do
+    { 
+      System.out.println("Level 1: Guess a random password of 8 characters, letters and numbers only (with hints):");
+      proceed = lvlOne.guess();
+      if(proceed)
+      {
+        System.out.println("Congratulations! You may now advance to...\nLevel 2: Guess a random 3-5 digit authentication code (with hints)."
+        proceed = lvl2.guess();
+      }
+      if(proceed)
+        System.out.println("Congratulations! You have solved the password guessing game.");
+      else
+        System.out.println("Give up? Better luck next time!");
+        
+      System.out.println("Would you like to play again? ('y'/'n')");
+      playFlag = io.nextLine.charAt(0);
+    } while(playFlag.equalsIgnoreCase('y'));
+  }
+}
