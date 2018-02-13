4a7d7f2f54c06d39f455491fa4d1baafc8c2ce1a
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-13 14:07:33 -0800

    Update TwoFactorAuth.java

--- TwoFactorAuth.java
+++ TwoFactorAuth.java
@@ -8,5 +8,9 @@ public TwoFactorAuth()
 public char[] generatePass() //overridden generatePass() method from superclass Password
 //in order to continue using char[] for the password, make sure to convert randomly generated ints to their character equivalents (heres the ASCII table again http://www.asciitable.com/)
 
+public boolean compare(char[] pass2) //overridden from Password class, use this to determine where the digits are incorrect etc basically the bulls and cows rules
+  {
+  }
+
 public boolean guess() //overridden from Superclass Password, prompts user to guess password til they get it (returns true if they do, false if they exit)
 }
