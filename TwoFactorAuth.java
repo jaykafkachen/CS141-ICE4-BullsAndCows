d41cd5881eb1c3ba7aa78fb2f0cefcac1c06b004
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-13 14:57:14 -0800

    Update TwoFactorAuth.java

--- TwoFactorAuth.java
+++ TwoFactorAuth.java
@@ -1,16 +1,25 @@
-Class TwoFactorAuth extends Password
+class TwoFactorAuth extends Password
 {
-public TwoFactorAuth()
-{
-  super(generatePass()); //calls the superclass constructor, passing in the char[] from generate pass
-}
-
-public char[] generatePass() //overridden generatePass() method from superclass Password
-//in order to continue using char[] for the password, make sure to convert randomly generated ints to their character equivalents (heres the ASCII table again http://www.asciitable.com/)
-
-public boolean compare(char[] pass2) //overridden from Password class, use this to determine where the digits are incorrect etc basically the bulls and cows rules
+  public TwoFactorAuth()
+  {
+    super(); 
+  }
+  
+  //overridden generatePass() method from superclass Password
+  //in order to continue using char[] for the password, make sure to convert random
+  //generated ints to their character equivalents (heres the ASCII table again http://www
+  //.asciitable.com/)
+  public char[] generatePass() 
+  {}
+  
+  //overridden from Password class, use this to 
+  //determine where the digits are incorrect etc basically the bulls and cows rules
+  public boolean compare(char[] pass2) 
   {
   }
-
-public boolean guess() //overridden from Superclass Password, prompts user to guess password til they get it (returns true if they do, false if they exit)
+  
+  //overridden from Superclass Password, prompts user to guess password til they get 
+  //(returns true if they do, false if they exit)
+  public boolean guess()
+  {}
 }
