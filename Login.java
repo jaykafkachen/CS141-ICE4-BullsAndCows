607fa9b8feb7525135ab4f9abce3046192b5e72e
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-15 23:47:48 -0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -15,10 +15,10 @@ public class Login extends Password
 		
 	public char[] generatePass() 
 	{
-		char[] password = new char[8];
+		char[] password = new char[6];
 		for(int i=0;i<password.length;i++)
 		{
-			password[i] = (char)(96 + (int)(Math.random() * ((122 - 96) + 1)));
+			password[i] = (char)(97 + (int)(Math.random() * ((122 - 97) + 1)));
 			//System.out.print(password[i]);
 		}
 		return password;
@@ -29,31 +29,45 @@ public class Login extends Password
 	public boolean guess() 
 	{
 		boolean end = false;
-		char[] passguess = new char[8];
-		char[] truepass = new char[8];
+		char[] passguess = new char[6];
+		char[] truepass = new char[6];
 		String input = "";
 		Scanner io = new Scanner(System.in);
 		while(end!=true)
 		{
-			System.out.print("\n Guess password-->");
+			System.out.print("\n Guess password\n-->");
 			input = io.nextLine();
-			while(input.length()!=8)
+			while(input.length()!=6)
 			{
-				System.out.print("Invalid input. Must be 8 letters.");
+				System.out.print("Invalid input. Must be 6 letters.");
 				System.out.print("\nGuess password\n-->");
 				input = io.nextLine();
 			}
 			passguess = input.toCharArray();
 			truepass = compare(passguess);
-			if(truepass==passguess)
+			if(truepass.toString().equals(passguess.toString()))
 				return true;
 			else
 			{
-				System.out.print("Correct so far: " + String.valueOf(truepass) + "\nCorrect letters: ");
-				//print out correct letters too
-				System.out.print("\nDo you want to quit? (y/n)\n-->");
-				if(io.nextLine().equalsIgnoreCase("y"))
-					end = true;
+				System.out.print("Correct so far: (Indexes gives you correct index (0-5) of letter entered)\nYours:   "+String.valueOf(passguess)+"\nIndexes: "); //+ String.valueOf(truepass));
+				for(char c:truepass)
+				{
+					if(Character.isDigit(c))
+						System.out.print(c);
+					else
+						System.out.print("-");
+				}
+				System.out.print("\nLetters: ");
+				for(char c:truepass)
+				{
+					if(Character.isLetter(c))
+						System.out.print(c);
+					else
+						System.out.print("-");
+				}
+				//System.out.print("\nDo you want to quit? (y/n)\n-->");
+				//if(io.nextLine().equalsIgnoreCase("y"))
+				//	end = true;
 			}
 		}
 		return false;
